a4a8331501873b66eec95a314803c3cfedad4fb0
Sarah Mattar <sarah.mattar@commit.dev>
2021-11-20 21:00:46 -0800

    Initial Commit

---
+++ YT-Vapor-iOS-Aapp/YT_Vapor_iOS_AappApp.swift
@@ -0,0 +1,17 @@
+//
+//  YT_Vapor_iOS_AappApp.swift
+//  YT-Vapor-iOS-Aapp
+//
+//  Created by Sarah Mattar on 2021-11-20.
+//
+
+import SwiftUI
+
+@main
+struct YT_Vapor_iOS_AappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
